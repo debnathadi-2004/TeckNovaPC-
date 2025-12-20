c46abe5ccafe7564a95c35e39b091629ae244dac
Adi Debnath <your-email@example.com>
2025-12-20 22:49:03 +0600

    Initial commit: full project with frontend and backend

---
+++ frontend/js/singup.js
@@ -0,0 +1,18 @@
+document.querySelector("#signupForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+
+  const res = await fetch("http://localhost:5000/api/auth/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name, email, password }),
+  });
+
+  const data = await res.json();
+  alert(data.message);
+
+  if (res.ok) window.location.href = "login.html";
+});
